36634c5e9828cb33d172631240ddc609547f2fb4
weronikakus <235909@edu.p.lodz.pl>
2022-05-16 16:42:27 +0200

    add project

---
+++ src/com/Client.java
@@ -0,0 +1,30 @@
+package com;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    private Socket socket;
+    private Scanner in;
+    private PrintWriter out;
+
+    public Client(String serverAddress) throws Exception {
+
+        socket = new Socket(serverAddress, 10);
+        in = new Scanner(socket.getInputStream());
+        out = new PrintWriter(socket.getOutputStream(), true);
+
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Enter IP address of host: ");
+        String ipAddress = scanner.nextLine();
+        Client client = new Client(ipAddress);
+
+    }
+}
+
---
+++ src/com/Server.java
@@ -0,0 +1,21 @@
+package com;
+
+import java.net.ServerSocket;
+import java.util.concurrent.Executors;
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        try (var listener = new ServerSocket(10)) {
+            System.out.println("Tic Tac Toe Server is Running...");
+            var pool = Executors.newFixedThreadPool(200);
+            while (true) {
+                Game game = new Game();
+                pool.execute(game.new Player(listener.accept(), 'X'));
+                System.out.println("Player one has join");
+                pool.execute(game.new Player(listener.accept(), 'O'));
+                System.out.println("Player two has join");
+            }
+        }
+    }
+}
+
